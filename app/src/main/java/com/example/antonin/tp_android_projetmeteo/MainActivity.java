9dbdaae8cdb572120149a230945dc04f8cc96591
Antonin SPONTAK <antonin.spontak@gmail.com>
2017-04-02 20:56:34 +0200

    Debut dev verif internet

--- app/src/main/java/com/example/antonin/tp_android_projetmeteo/MainActivity.java
+++ app/src/main/java/com/example/antonin/tp_android_projetmeteo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.antonin.tp_android_projetmeteo;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -13,6 +14,13 @@ import java.util.List;
 // http://api.openweathermap.org/data/2.5/forecast?q=villeurbanne,69100&APPID=910f0c05f62e5508a3428198252eed06&units=metric
 // http://api.androidhive.info/contacts/
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+
     private static String url = "http://api.openweathermap.org/data/2.5/forecast?q=villeurbanne,69100&APPID=910f0c05f62e5508a3428198252eed06&units=metric";
     private ListView lv;
     /*
